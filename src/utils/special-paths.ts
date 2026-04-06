7898061716f90ef4c1ae0b7e84b0260f8a51978c
Tim Kelly <timlkelly@users.noreply.github.com>
2026-04-06 08:43:25 -0500

    Use localhostMatch host

--- src/utils/special-paths.ts
+++ src/utils/special-paths.ts
@@ -183,7 +183,7 @@ export function parseFileNameFromSymbolication(
     const { host, path } = localhostMatch.groups;
     return {
       type: 'localhost',
-      host: location.host + '/source',
+      host: host + '/source',
       path,
     };
   }
